494892d5dd18256f8cb7043b7dfc886c250c89f7
stripedtshirt <jerryzenret@gmail.com>
2025-11-15 18:59:30 +0100

    fixed the notification panel so that there are buttons that control the movement of the images

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// sidebar logic
+
 const hamburger = document.getElementById('hamburger');
 const closeHamburger = document.getElementById('close-hamburger');
 
@@ -12,4 +14,50 @@ function hideSidebar() {
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
+}
+
+// sliding images logic
+
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId = null;
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+next.addEventListener('click', nextSlide)
+prev.addEventListener('click', prevSlide)
+
+document.addEventListener('DOMContentLoaded', initializeSlider)
+
+function initializeSlider() {
+    if(slides.length > 0) {
+    slides[slideIndex].classList.add('displaySlide');
+    intervalId = setInterval(nextSlide, 4000);
+    }
+}
+function showSlide(index) {
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+
+}
+
+function prevSlide() {
+    // clearInterval(intervalId)
+    slideIndex--;
+    showSlide(slideIndex);
+}
+function nextSlide() {
+    slideIndex++
+    showSlide(slideIndex);
 }
